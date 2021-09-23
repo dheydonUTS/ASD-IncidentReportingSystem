68ecdfe5111db655509f563dc887e5baf59e4c22
Joe (Windows Desktop) <joe.drew@yahoo.com.au>
2021-09-23 12:12:36 +1000

    Working Test Email Servlet

--- src/main/java/controller/TestEmailServlet.java
+++ src/main/java/controller/TestEmailServlet.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
+import com.sun.mail.util.MailSSLSocketFactory;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -13,14 +9,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.security.GeneralSecurityException;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.mail.*;
 import javax.mail.internet.*;
 import javax.activation.*;
 
 /**
  *
- * @author joeda
+ * @author joe
  */
 @WebServlet(name = "TestEmailServlet", urlPatterns = {"/TestEmailServlet"})
 public class TestEmailServlet extends HttpServlet {
@@ -30,11 +29,20 @@ public class TestEmailServlet extends HttpServlet {
         final String password = "IncidentRS!1";
 
         Properties prop = new Properties();
-        prop.put("mail.smtp.host", "smtp.gmail.com");
-        prop.put("mail.smtp.port", "465");
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
         prop.put("mail.smtp.auth", "true");
-        //prop.put("mail.smtp.socketFactory.port", "465");
-       // prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        prop.put("mail.smtp.starttls.enable", "true"); //TLS
+        MailSSLSocketFactory sf;
+           try {
+               sf = new MailSSLSocketFactory();
+               sf.setTrustAllHosts(true); 
+                prop.put("mail.smtp.ssl.trust", "*");
+                prop.put("mail.smtp.ssl.socketFactory", sf);
+           } catch (GeneralSecurityException ex) {
+               Logger.getLogger(TestEmailServlet.class.getName()).log(Level.SEVERE, null, ex);
+           }
+
         
         Session session = Session.getInstance(prop,
                 new javax.mail.Authenticator() {
@@ -42,20 +50,19 @@ public class TestEmailServlet extends HttpServlet {
                         return new PasswordAuthentication(username, password);
                     }
                 });
-        session.setDebug(true);
+
         try {
 
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("incidentrs@gmail.com"));
+            message.setFrom(new InternetAddress("from@gmail.com"));
             message.setRecipients(
                     Message.RecipientType.TO,
                     InternetAddress.parse("joedahackinbot@gmail.com, joe.drew@yahoo.com.au")
             );
-            message.setSubject("Testing Gmail SSL");
+            message.setSubject("Testing Gmail TLS");
             message.setText("Dear Mail Crawler,"
                     + "\n\n Please do not spam my email!");
 
-   
             Transport.send(message);
 
             System.out.println("Done");
@@ -63,6 +70,5 @@ public class TestEmailServlet extends HttpServlet {
         } catch (MessagingException e) {
             e.printStackTrace();
         }
-       }
-
+}
 }
