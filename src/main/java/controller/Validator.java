546568e1706a8dd0fe581dfc275ea6749a88e3e4
dheydonUTS <79435846+dheydonUTS@users.noreply.github.com>
2021-10-06 15:23:24 +1100

    Changes

--- src/main/java/controller/Validator.java
+++ src/main/java/controller/Validator.java
@@ -9,7 +9,7 @@ import java.util.regex.Pattern;
 public class Validator {
     private Pattern emailPattern = Pattern.compile("([a-zA-Z0-9._-]+)(@)([a-zA-Z0-9]+)(.)([a-zA-Z0-9._-]+)"); // Emails only
     private Pattern passwordPattern = Pattern.compile("([a-zA-Z0-9#&*<>/,.%()^?@_!$]{6,32}+)");//6-32 reg characters with at least 1 special char
-    private Pattern namePattern = Pattern.compile("([a-zA-Z]{1,50})");          // Letters only  1 to 50 characters
+    private Pattern namePattern = Pattern.compile("([a-zA-Z\\s]{1,50})");          // Letters only  1 to 50 characters
     private Pattern descPattern = Pattern.compile("([a-zA-Z0-9\\s]{1,1000})");  // Letters number and whitespaces - 1 to 1000 characters
     private Pattern lonLatPattern = Pattern.compile("([0-9]+)(.)([0-9]+)");     // Numbers with a decimal point in between 
 
