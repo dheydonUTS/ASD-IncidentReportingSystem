8dcf304edb8fce848a3d92894c65aa9ed6fded4b
Joe (Windows Desktop) <joe.drew@yahoo.com.au>
2021-10-24 20:49:05 +1100

    Fixed db details

--- src/main/java/model/dao/DB.java
+++ src/main/java/model/dao/DB.java
@@ -7,7 +7,7 @@ package model.dao;
 import java.sql.*;
 
 public abstract class DB {
-    protected String url="jdbc:derby:";
+    protected String url="jdbc:derby://localhost:1527/";
     protected String db = "incidentrs";
     protected String dbuser = "incidentrs";
     protected String dbpass = "password123";
