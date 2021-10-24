899210f3b85b3b19b579ea69e88c0b3a2641a53a
Joe (Windows Desktop) <joe.drew@yahoo.com.au>
2021-10-24 19:22:06 +1100

    Update DB.java

--- src/main/java/model/dao/DB.java
+++ src/main/java/model/dao/DB.java
@@ -9,8 +9,8 @@ import java.sql.*;
 public abstract class DB {
     protected String url="jdbc:derby://localhost:1527/";
     protected String db = "incidentrs";
-    protected String dbuser = "incidentrs";
-    protected String dbpass = "password123";
+    protected String dbuser = "";
+    protected String dbpass = "";
     protected String driver = "org.apache.derby.jdbc.ClientDriver";
     protected Connection con;
 }
